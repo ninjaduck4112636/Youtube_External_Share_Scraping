dc2f301965982b140e9cd3c4657bb5cebf20a2c3
ninjaduck4112636 <ninjaduck4112636@gmail.com>
2020-11-24 06:34:06 -0800

    written logic to find all the links in the video section

--- main.py
+++ main.py
@@ -9,9 +9,20 @@ try:
     driver.get("https://www.youtube.com/channel/UCqL4rm4nwqhjsOOpFn4Bg7A/videos")
     time.sleep(40)
 
-    ihref = driver.find_element_by_xpath('//a[@id="video-title"]').get_attribute("href")
-    time.sleep(5)
-    print(ihref)
+    counter = 1;
+    istring = '//a[@id="video-title"]'
+    x_path_details = driver.find_element_by_xpath(istring)
+    
+    while True:
+        if(counter==1):
+            ihref = x_path_details.get_attribute("href")
+        else:
+            istring += '/following::a/following::a'
+            ihref = driver.find_element_by_xpath(istring).get_attribute("href")
+        time.sleep(5)
+        print(ihref)
+        counter += 1
+        
 except Exception as e:
     print(e)
 finally:
